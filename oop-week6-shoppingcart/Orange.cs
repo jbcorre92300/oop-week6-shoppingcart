88551c3e7beab2bfcd66c0b0595129edecb24e9e
JB Corre <23182@student.dorset-college.ie>
2020-10-21 15:16:45 +0100

    Ajoutez des fichiers projet.

---
+++ oop-week6-shoppingcart/Orange.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace oop_week6_shoppingcart
+{
+    class Orange
+    {
+    }
+}
---
+++ oop-week6-shoppingcart/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace oop_week6_shoppingcart
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
